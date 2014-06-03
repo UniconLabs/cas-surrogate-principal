6c5c054852c602b5b868051f622e8720d5847226
Jj <jj@scaldingspoon.com>
2014-06-03 16:18:11 -0500

    [#1]

--- src/main/java/edu/berkeley/cas/authentication/handler/SurrogateUsernamePasswordAuthenticationHandler.java
+++ src/main/java/edu/berkeley/cas/authentication/handler/SurrogateUsernamePasswordAuthenticationHandler.java
@@ -1,20 +1,60 @@
 package edu.berkeley.cas.authentication.handler;
 
-import org.jasig.cas.authentication.principal.Credentials;
-import org.jasig.cas.authentication.handler.AuthenticationHandler;
+import edu.berkeley.cas.authentication.service.SurrogateUsernamePasswordService;
 import org.jasig.cas.authentication.handler.AuthenticationException;
+import org.jasig.cas.authentication.handler.AuthenticationHandler;
+import org.jasig.cas.authentication.principal.Credentials;
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
 
 /**
  * Class to handle surrogate authentication
  */
 public class SurrogateUsernamePasswordAuthenticationHandler implements AuthenticationHandler {
+    java.util.List<org.jasig.cas.authentication.handler.AuthenticationHandler> authenticationHandlerList;
+    SurrogateUsernamePasswordService surrogateUsernamePasswordService;
+
+    /**
+     * The String that separates the parts of the username, eg "+" in "group+username"
+     */
+    String separator = "+";
+
     @Override
     public boolean authenticate(Credentials credentials) throws AuthenticationException {
-        throw new UnsupportedOperationException("not yet implemented");
+        // TODO: this is not safe! we should explore making credentials cloneable!
+        UsernamePasswordCredentials usernamePasswordCredentials = (UsernamePasswordCredentials) credentials;
+        boolean value = false;
+        String passedUsername = usernamePasswordCredentials.getUsername();
+        String surrogateUsername = passedUsername.substring(passedUsername.indexOf(separator) + 1);
+        String targetUsername = passedUsername.substring(0, passedUsername.indexOf(separator));
+        usernamePasswordCredentials.setUsername(surrogateUsername);
+        for (AuthenticationHandler handler : this.authenticationHandlerList) {
+            if (handler.supports(usernamePasswordCredentials)
+                    && handler.authenticate(usernamePasswordCredentials)
+                    && this.surrogateUsernamePasswordService.canAuthenticateAs(targetUsername, surrogateUsername)) {
+                value = true;
+                break;
+            }
+        }
+        usernamePasswordCredentials.setUsername(passedUsername);
+        return value;
     }
 
     @Override
     public boolean supports(Credentials credentials) {
-        throw new UnsupportedOperationException("not yet implemented");
+        return credentials != null
+                && UsernamePasswordCredentials.class.isAssignableFrom(credentials.getClass())
+                && ((UsernamePasswordCredentials) credentials).getUsername().contains(separator);
+    }
+
+    public void setAuthenticationHandlerList(java.util.List<AuthenticationHandler> authenticationHandlerList) {
+        this.authenticationHandlerList = authenticationHandlerList;
+    }
+
+    public void setSurrogateUsernamePasswordService(SurrogateUsernamePasswordService surrogateUsernamePasswordService) {
+        this.surrogateUsernamePasswordService = surrogateUsernamePasswordService;
+    }
+
+    void setSeparator(String separator) {
+        this.separator = separator;
     }
 }
--- src/test/groovy/edu/berkeley/cas/authentication/handler/SurrogateUsernamePasswordAuthenticationHandlerSpec.groovy
+++ src/test/groovy/edu/berkeley/cas/authentication/handler/SurrogateUsernamePasswordAuthenticationHandlerSpec.groovy
@@ -1,6 +1,60 @@
 package edu.berkeley.cas.authentication.handler
 
+import edu.berkeley.cas.authentication.service.SurrogateUsernamePasswordService
+import org.jasig.cas.authentication.handler.AuthenticationHandler
+import org.jasig.cas.authentication.principal.Credentials
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class SurrogateUsernamePasswordAuthenticationHandlerSpec extends Specification {
+    @Shared
+    SurrogateUsernamePasswordAuthenticationHandler surrogateUsernamePasswordAuthenticationHandler
+
+    void setupSpec() {
+        surrogateUsernamePasswordAuthenticationHandler = new SurrogateUsernamePasswordAuthenticationHandler(
+                surrogateUsernamePasswordService: [canAuthenticateAs: { target, surrogate ->
+                    return true
+                }] as SurrogateUsernamePasswordService,
+                authenticationHandlerList: [
+                        [
+                                authenticate: { it.username == it.password },
+                                supports    : { return UsernamePasswordCredentials.isAssignableFrom(it.class) }
+                        ] as AuthenticationHandler
+                ]
+        )
+    }
+
+    @Unroll
+    def "test supports"() {
+        expect:
+        surrogateUsernamePasswordAuthenticationHandler.supports(a) == b
+        where:
+        a                                                                    | b
+        [] as Credentials                                                    | false
+        new UsernamePasswordCredentials(username: "test", password: "test")  | false
+        new UsernamePasswordCredentials(username: "test+me", password: "me") | true
+    }
+
+    @Unroll
+    def "test different separators"() {
+        expect:
+        a.supports(b) == c
+        where:
+        a                                                                  | b                                                    | c
+        new SurrogateUsernamePasswordAuthenticationHandler()               | new UsernamePasswordCredentials(username: "test+me") | true
+        new SurrogateUsernamePasswordAuthenticationHandler(separator: "-") | new UsernamePasswordCredentials(username: "test+me") | false
+        new SurrogateUsernamePasswordAuthenticationHandler(separator: "-") | new UsernamePasswordCredentials(username: "test-me") | true
+    }
+
+    @Unroll
+    def "test authenticate"() {
+        expect:
+        surrogateUsernamePasswordAuthenticationHandler.authenticate(a) == b
+        where:
+        a                                                                       | b
+        new UsernamePasswordCredentials(username: "test+me", password: "me")    | true
+        new UsernamePasswordCredentials(username: "test+me", password: "wrong") | false
+    }
 }
