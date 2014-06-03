a4bd49d2d5c418da441102cf8b1fc40077a2867d
Jj <jj@scaldingspoon.com>
2014-06-03 17:53:37 -0500

    [#2 resolved]

--- src/main/java/edu/berkeley/cas/authentication/principal/SurrogateUsernamePasswordCredentialsToPrincipalResolver.java
+++ src/main/java/edu/berkeley/cas/authentication/principal/SurrogateUsernamePasswordCredentialsToPrincipalResolver.java
@@ -2,18 +2,31 @@ package edu.berkeley.cas.authentication.principal;
 
 import org.jasig.cas.authentication.principal.AbstractPersonDirectoryCredentialsToPrincipalResolver;
 import org.jasig.cas.authentication.principal.Credentials;
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
 
 /**
  * Class to resolve surrogate principals
  */
 public class SurrogateUsernamePasswordCredentialsToPrincipalResolver extends AbstractPersonDirectoryCredentialsToPrincipalResolver {
+    /**
+     * The String that separates the parts of the username, eg "+" in "group+username"
+     */
+    String separator = "+";
+
     @Override
     protected String extractPrincipalId(Credentials credentials) {
-        throw new UnsupportedOperationException("not yet implemented");
+        String passedUsername = ((UsernamePasswordCredentials) credentials).getUsername();
+        return passedUsername.substring(passedUsername.indexOf(this.separator) + 1);
     }
 
     @Override
     public boolean supports(Credentials credentials) {
-        throw new UnsupportedOperationException("not yet implemented");
+        return credentials != null
+                && UsernamePasswordCredentials.class.isAssignableFrom(credentials.getClass())
+                && ((UsernamePasswordCredentials) credentials).getUsername().contains(separator);
+    }
+
+    public void setSeparator(String separator) {
+        this.separator = separator;
     }
 }
--- src/test/groovy/edu/berkeley/cas/authentication/principal/SurrogateUsernamePasswordCredentialsToPrincipalResolverSpec.groovy
+++ src/test/groovy/edu/berkeley/cas/authentication/principal/SurrogateUsernamePasswordCredentialsToPrincipalResolverSpec.groovy
@@ -1,6 +1,31 @@
 package edu.berkeley.cas.authentication.principal
 
+import edu.berkeley.cas.authentication.handler.SurrogateUsernamePasswordAuthenticationHandler
+import org.jasig.cas.authentication.principal.Credentials
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class SurrogateUsernamePasswordCredentialsToPrincipalResolverSpec extends Specification {
+    @Unroll
+    def "test supports"() {
+        expect:
+        a.supports(b) == c
+        where:
+        a                                                                           | b                                                    | c
+        new SurrogateUsernamePasswordCredentialsToPrincipalResolver()               | [:] as Credentials                                   | false
+        new SurrogateUsernamePasswordCredentialsToPrincipalResolver()               | new UsernamePasswordCredentials(username: "test")    | false
+        new SurrogateUsernamePasswordCredentialsToPrincipalResolver()               | new UsernamePasswordCredentials(username: "test+me") | true
+        new SurrogateUsernamePasswordCredentialsToPrincipalResolver(separator: '-') | new UsernamePasswordCredentials(username: "test-me") | true
+    }
+
+    @Unroll
+    def "test extract principal Id"() {
+        expect:
+        a.extractPrincipalId(b) == c
+        where:
+        a                                                                           | b                                                    | c
+        new SurrogateUsernamePasswordCredentialsToPrincipalResolver()               | new UsernamePasswordCredentials(username: "test+me") | "me"
+        new SurrogateUsernamePasswordCredentialsToPrincipalResolver(separator: '-') | new UsernamePasswordCredentials(username: "test-me") | "me"
+    }
 }
