c8ae83c29a04657c97e3cb9d9202519b565c2542
Matt Glazier <matthewglazier@gmail.com>
2014-06-13 12:08:36 -0700

    ldap search additions

--- src/main/java/edu/berkeley/cas/authentication/service/LdapSurrogateUsernamePasswordService.java
+++ src/main/java/edu/berkeley/cas/authentication/service/LdapSurrogateUsernamePasswordService.java
@@ -5,6 +5,15 @@ import org.springframework.ldap.core.ContextSource;
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.List;
+import javax.naming.ldap.LdapName;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ldap.core.ContextMapper;
+import org.springframework.ldap.core.DirContextOperations;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.AbstractContextMapper;
 
 /**
  * Surrogate username password service that uses LDAP
@@ -15,8 +24,56 @@ public class LdapSurrogateUsernamePasswordService implements SurrogateUsernamePa
 
     String baseDN = "";
     String userFilter = "(uid=%u)";
+    
+    @Value("${ldap.surrogate.search.filter}")
+    private String surrogateSearchFilter;
+    
+    @Value("${ldap.user.search.filter}")
+    private String userSearchFilter;
+    
+    @Value("${ldap.group.search.filter}")
+    private String groupSearchFilter;
+    
+    @Autowired
+    private LdapTemplate ldapTemplate;
+    
     @Override
     public boolean canAuthenticateAs(String username, String surrogate) {
+        try {                
+            ContextMapper mapper = new AbstractContextMapper() {
+                @Override
+                public Object doMapFromContext( DirContextOperations ctx) {
+                    return ctx.getDn();
+                }
+            };
+                    
+            // get the dn of the surrogate user
+            String filter = StringUtils.replace( surrogateSearchFilter, "?", surrogate);
+            List surrogateUser = ldapTemplate.search( "", filter, mapper);
+            
+            // get the dn of the user
+            filter = StringUtils.replace( userSearchFilter, "?", username);
+            List userList = ldapTemplate.search( "", filter, mapper);
+            
+            if(( surrogateUser != null && surrogateUser.size() > 0) && 
+                    ( userList != null && userList.size() > 0)) {
+                LdapName ldapUser = (LdapName)userList.get(0);
+                LdapName ldapSurrogate = (LdapName)surrogateUser.get(0);
+                
+                // find any group that has the user and surrogate as members
+                // If a group is found, then return true.
+                filter = StringUtils.replace( groupSearchFilter, "$1", ldapUser.toString());
+                filter = StringUtils.replace( filter, "$2", ldapSurrogate.toString());
+                List groups = ldapTemplate.search( "", filter, mapper);
+            
+                if( groups != null && groups.size() > 0) {
+                    return true;
+                }
+            }
+        }
+        catch( Exception e) {
+        }
+       
         return false;
     }
 
