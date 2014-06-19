d5e46f554a1d2a19a24cbfadc33a6c1942c0b2d8
Jj <jj@scaldingspoon.com>
2014-06-19 10:09:08 -0500

    [NOISSUE] fix for new credential type

--- src/main/java/edu/berkeley/cas/authentication/principal/SurrogateUsernamePasswordCredentialsToPrincipalResolver.java
+++ src/main/java/edu/berkeley/cas/authentication/principal/SurrogateUsernamePasswordCredentialsToPrincipalResolver.java
@@ -2,33 +2,18 @@ package edu.berkeley.cas.authentication.principal;
 
 import org.jasig.cas.authentication.principal.AbstractPersonDirectoryCredentialsToPrincipalResolver;
 import org.jasig.cas.authentication.principal.Credentials;
-import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
-import org.springframework.beans.factory.annotation.Value;
 
 /**
  * Class to resolve surrogate principals
  */
 class SurrogateUsernamePasswordCredentialsToPrincipalResolver extends AbstractPersonDirectoryCredentialsToPrincipalResolver {
-    /**
-     * The String that separates the parts of the username, eg "+" in "group+username"
-     */
-    @Value("${surrogate.username.separator}")
-    private String separator = "+";
-
     @Override
     protected String extractPrincipalId(Credentials credentials) {
-        String passedUsername = ((UsernamePasswordCredentials) credentials).getUsername();
-        return passedUsername.substring(0, passedUsername.indexOf(this.separator));
+        return ((SurrogateUsernamePasswordCredentials)credentials).getTargetUsername();
     }
 
     @Override
     public boolean supports(Credentials credentials) {
-        return credentials != null
-                && UsernamePasswordCredentials.class.isAssignableFrom(credentials.getClass())
-                && ((UsernamePasswordCredentials) credentials).getUsername().contains(separator);
-    }
-
-    public void setSeparator(String separator) {
-        this.separator = separator;
+        return credentials != null && SurrogateUsernamePasswordCredentials.class.isAssignableFrom(credentials.getClass());
     }
 }
